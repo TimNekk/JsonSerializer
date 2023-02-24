0a3ca3503f88e00d567dfc08a6817a7e672632ef
Tim <herew26@gmail.com>
2023-02-24 23:30:12 +0300

    feat: add custom exception

---
+++ src/main/java/timnekk/SerializationException.java
@@ -0,0 +1,19 @@
+package timnekk;
+
+public class SerializationException extends Exception {
+    public SerializationException(String message) {
+        super(message);
+    }
+
+    public SerializationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public SerializationException(Throwable cause) {
+        super(cause);
+    }
+
+    public SerializationException() {
+        super();
+    }
+}
